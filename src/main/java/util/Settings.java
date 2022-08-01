ece5b9acfdae3bff01ec505082a433f1b18f8f75
Makkkkus <37084190+Makkkkus@users.noreply.github.com>
2022-08-01 19:24:28 +0200

    Build: Increment version

--- src/main/java/util/Settings.java
+++ src/main/java/util/Settings.java
@@ -24,7 +24,7 @@ public class Settings {
 
 	public static final String NODEGUILD = "318668421719916545"; //W:286194821394071552 E:318668421719916545
 	public static final long STARTTIME = System.nanoTime();
-	public static final String VERSION = "1.3.2";
+	public static final String VERSION = "1.4.0";
 
 	public static String prefix = "ed!";
 	public static boolean debug = false;
